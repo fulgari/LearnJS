f356fd199f7d6b8ddbb113d5024932695e14865b
Johnny Pan <swanf@outlook.com>
2022-05-13 16:34:58 +0800

    article: update crdt

--- InterviewQnA/draggable和事件.md
+++ InterviewQnA/draggable和事件.md
@@ -1,14 +1,15 @@
-### 拖拽属性（HTML5新增）
+### 拖拽属性（HTML5 新增）
+
 拖拽属性能够注册拖动事件。
 
 - 拖动时（点击鼠标，移动，放开鼠标）：
-    1）点击鼠标开始：dragstart 
-    2）按住鼠标拖行：drag 
-    3）松开鼠标结束： dragend
-- 进入区域： 
-    1）进入时：dragenter --进入时发生的
-    2）进入后：dragover --进入后在相应的区域内发生的
-    3）离开：dragleave 
-    4）放下：drop
+  1）点击鼠标开始：dragstart --只在拖动开始时触发；`e.dataTransfer.setData('text/plain', <some data string>);` 设置拖动数据；`e.dataTransfer.setDragImage(<img>, <x>, <y>);` 设置拖动图像；
+  2）按住鼠标拖行：drag --会一直不停触发
+  3）松开鼠标结束： dragend
+- 进入区域：
+  1）进入时：dragenter --进入时发生的
+  2）进入后：dragover --进入后在相应的区域内发生的, 默认不触发事件，需要 `preventDefault` 才能放置拖拽的数据
+  3）离开：dragleave
+  4）放下：drop
 
 鼠标事件：mouseenter, mousemove(会不断触发), mouseleave, mousedown, mouseup, mouseover, mouseout, click, dbclick
--- InterviewQnA/promise.js
+++ InterviewQnA/promise.js
@@ -54,250 +54,261 @@ Promise.race_ = function(promises) {
 /********************/
 //ES6 的模块自动开启严格模式，不管你有没有在模块头部加上 use strict;。
 class Promise {
-	constructor(fn) {
-		/*note: Promise核心的私有变量(用this)：状态+两个list */
-		this.status = 'pending' //三种状态：pending，fulfilled，rejected
-		this.resolveList = [] //成功后的回调函数
-		this.rejectList = [] //失败后的cb
+  constructor(fn) {
+    /*note: Promise核心的私有变量(用this)：状态+两个list */
+    this.status = "pending"; //三种状态：pending，fulfilled，rejected
+    this.resolveList = []; //成功后的回调函数
+    this.rejectList = []; //失败后的cb
 
-		/*note：用下面自己写的resolve/reject作参（注意要bind(this)否则这两个函数内的this指针不是指向new出来的Promise而是window或(strict模式)undefined）调用fn*/
-		fn(this.resolve.bind(this), this.reject.bind(this))
-	}
+    /*note：用下面自己写的resolve/reject作参（注意要bind(this)否则这两个函数内的this指针不是指向new出来的Promise而是window或(strict模式)undefined）调用fn*/
+    fn(this.resolve.bind(this), this.reject.bind(this));
+  }
 
-	// then(success callback, failure callback)
-	then(scb, fcb) {
-		if(scb) {
-			this.resolveList.push(scb)
-		}
-		if(fcb) {
-			this.rejectList.push(fcb)
-		}
-		return this //返回当前Promise对象
-	}
+  // then(success callback, failure callback)
+  then(scb, fcb) {
+    if (scb) {
+      this.resolveList.push(scb);
+    }
+    if (fcb) {
+      this.rejectList.push(fcb);
+    }
+    return this; //返回当前Promise对象
+  }
 
-	catch(cb) {
-		if(cb) {
-			this.rejectList.push(cb)
-		}
-		return this
-	}
+  catch(cb) {
+    if (cb) {
+      this.rejectList.push(cb);
+    }
+    return this;
+  }
 
-	resolve(data) {
-		if(this.status !== 'pending') {
-			return
-		}
-		this.status = 'fulfilled'
-		setTimeout(() => {
-			this.resolveList.forEach(s => {
-				s(data)
-			})
-		})
-	}
+  resolve(data) {
+    if (this.status !== "pending") {
+      return;
+    }
+    this.status = "fulfilled";
+    setTimeout(() => {
+      this.resolveList.forEach((s) => {
+        s(data);
+      });
+    });
+  }
 
-	reject(err) {
-		if(this.status !== 'pending') {
-			return
-		}
-		this.status = 'rejected'
-		setTimeout(() => {
-			this.rejectList.forEach(f => {
-				f(err)
-			})
-		})
-	}
+  reject(err) {
+    if (this.status !== "pending") {
+      return;
+    }
+    this.status = "rejected";
+    setTimeout(() => {
+      this.rejectList.forEach((f) => {
+        f(err);
+      });
+    });
+  }
 
+  /**
+   * 实现Promise.resolve
+   * 1.参数是一个 Promise 实例, 那么Promise.resolve将不做任何修改、原封不动地返回这个实例。
+   * 2.如果参数是一个原始值，或者是一个不具有then方法的对象，则Promise.resolve方法返回一个新的 Promise 对象，状态为resolved。
+   */
+  static resolve(data) {
+    if (data instanceof Promise) {
+      return data;
+    } else {
+      return new Promise((resolve, reject) => {
+        resolve(data);
+      });
+    }
+  }
 
-    /**
-     * 实现Promise.resolve
-     * 1.参数是一个 Promise 实例, 那么Promise.resolve将不做任何修改、原封不动地返回这个实例。
-     * 2.如果参数是一个原始值，或者是一个不具有then方法的对象，则Promise.resolve方法返回一个新的 Promise 对象，状态为resolved。
-    */ 
-	static resolve(data) {
-		if(data instanceof Promise) {
-			return data
-		} else {
-			return new Promise((resolve, reject) => {
-				resolve(data)
-			})
-		}
-	}
+  static reject(err) {
+    if (err instanceof Promise) {
+      return err;
+    } else {
+      return new Promise((resolve, reject) => {
+        reject(err);
+      });
+    }
+  }
+  /**
+   * 实现Promise.all
+   * 1. Promise.all方法接受一个数组作为参数，p1、p2、p3都是 Promise 实例，如果不是，就会先调用下面讲到的Promise.resolve方法，将参数转为 Promise 实例，再进一步处理。
+   * 2. 返回值组成一个数组
+   */
+  static all(promises) {
+    return new Promise((resolve, reject) => {
+      promises = Array.from(promises);
+      let count = 0;
+      let res = [];
+      let length = promises.length;
 
-	static reject(err) {
-		if(err instanceof Promise) {
-			return err
-		} else {
-			return new Promise((resolve, reject) => {
-				reject(err)
-			})
-		}
-	}
-    /**
-     * 实现Promise.all
-     * 1. Promise.all方法接受一个数组作为参数，p1、p2、p3都是 Promise 实例，如果不是，就会先调用下面讲到的Promise.resolve方法，将参数转为 Promise 实例，再进一步处理。
-     * 2. 返回值组成一个数组
-    */
-	static all(promises) {
-		return new Promise((resolve, reject) => {
-			promises = Array.from(promises)
-			let count = 0
-			let res = []
-			let length = promises.length
-
-			if(length === 0) {
-				resolve([])
-			} else {
-				for(let i=0; i<length; i++) {
-					// 考虑到promise[i]可能是thenable对象也可能是普通值
-					Promise.resolve(promises[i]).then(data => {
-						res[i] = data
-						if(++count === length) {
-							resolve(res)
-						}
-					}, err => {
-						reject(err)
-						return //Promise.all一旦有一个err就会reject(err)并直接返回
-					})
-				}
-			}
-
-		})
-	}
-    /**
-     * 实现Promise.race
-     * 1. Promise.race方法的参数与Promise.all方法一样，如果不是 Promise 实例，就会先调用下面讲到的Promise.resolve方法，将参数转为 Promise 实例，再进一步处理。
-     * 2. 返回那个率先改变的 Promise 实例的返回值
-    */
-	static race(promises) {
-		return new Promise((resolve, reject) => {
-			promises = Array.from(promises)
-			if(promises.length === 0) {
-				return
-			} else {
-				for(let i=0; i<promises.length; i++) {
-					Promise.resolve(promises[i]).then(data => {
-						resovle(data)
-						return
-					}, err => {
-						reject(err)
-						return //一旦有resolve或reject都会直接返回，停止执行了
-					})
-				}
-			}
-		})
-	}
+      if (length === 0) {
+        resolve([]);
+      } else {
+        for (let i = 0; i < length; i++) {
+          // 考虑到promise[i]可能是thenable对象也可能是普通值
+          Promise.resolve(promises[i]).then(
+            (data) => {
+              res[i] = data;
+              if (++count === length) {
+                resolve(res);
+              }
+            },
+            (err) => {
+              reject(err);
+              return; //Promise.all一旦有一个err就会reject(err)并直接返回
+            }
+          );
+        }
+      }
+    });
+  }
+  /**
+   * 实现Promise.race
+   * 1. Promise.race方法的参数与Promise.all方法一样，如果不是 Promise 实例，就会先调用下面讲到的Promise.resolve方法，将参数转为 Promise 实例，再进一步处理。
+   * 2. 返回那个率先改变的 Promise 实例的返回值
+   */
+  static race(promises) {
+    return new Promise((resolve, reject) => {
+      promises = Array.from(promises);
+      if (promises.length === 0) {
+        return;
+      } else {
+        for (let i = 0; i < promises.length; i++) {
+          Promise.resolve(promises[i]).then(
+            (data) => {
+              resovle(data);
+              return;
+            },
+            (err) => {
+              reject(err);
+              return; //一旦有resolve或reject都会直接返回，停止执行了
+            }
+          );
+        }
+      }
+    });
+  }
 }
 
 const p = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        console.log('resolve');
-        resolve(222);
-    }, 1000)
-})
-
-p.then(data => {
-    setTimeout(() => {
-        console.log('data', data);
-    })
-    return 3333;
-}).then(data2 => {
-    console.log('data2', data2);
-}).catch(err => {
-    console.error('err', err);
+  setTimeout(() => {
+    console.log("resolve");
+    resolve(222);
+  }, 1000);
 });
 
-/*练习默写*/
-class Promise1{
-	constructor(fn) {
-		this.status = 'pending'
-		this.resolveList = []
-		this.rejectList = []
-	}
-	then(scb, fcb) {
-		if(scb) {
-			this.resolveList.push(scb)
-		}
-		if(fcb) {
-			this.rejectList.push(fcb)
-		}
-		return this
-	}
-	catch(fcb) {
-		if(fcb) {
-			this.rejectList.push(fcb)
-		}
-		return this
-	}
-	resolve(data) {
-		if(this.status !== 'pending') {
-			return
-		}
-		this.status = 'fulfilled'
-		setTimeout(()=>{
-			this.resolveList.forEach(s=>{
-				s(data)
-			})
-		})
-	}
-	reject(err) {
-		if(this.status !== 'pending') {
-			return
-		}
-		this.status = 'rejected'
-		setTimeout(()=>{
-			this.rejectList.forEach(f=>{
-				f(err)
-			})
-		})
-	}
-	static resolve(data) {
-		if(data instanceof Promise) {
-			return data
-		} else {
-			resolve(data)
-		}
-	}
-	static reject(err) {
-		if(err instanceof Promise) {
-			return err
-		} else {
-			reject(err)
-		}
-	}
-	static all(promises) {
-		return new Promise((resolve, reject)=>{
-			promises = Array.from(promises)
-			let count = 0
-			let length = promises.length
-			let result = []
-			for(let i=0; i<length; i++) {
-				Promise.resolve(promises[i]).then(data=>{
-					result[i] = data
-					++count
-					if(count === length){
-						resolve(result)
-					}
-				}, err=>{
-					reject(err)
-					return
-				})
-			}
-		})
-	}
+p.then((data) => {
+  setTimeout(() => {
+    console.log("data", data);
+  });
+  return 3333;
+})
+  .then((data2) => {
+    console.log("data2", data2);
+  })
+  .catch((err) => {
+    console.error("err", err);
+  });
 
-	static race(promises) {
-		return new Promise((resolve, reject)=>{
-			promises = Array.from(promises)
-			let length = promises.length
-			for(let i=0; i<length; i++) {
-				Promise.resolve(promises[i]).then(data=>{
-					resolve(data)
-					return
-				}, err=>{
-					reject(err)
-					return
-				})
-			}
-		})
-	}
-		
+/*练习默写*/
+class Promise1 {
+  constructor(fn) {
+    this.status = "pending";
+    this.resolveList = [];
+    this.rejectList = [];
+  }
+  then(scb, fcb) {
+    if (scb) {
+      this.resolveList.push(scb);
+    }
+    if (fcb) {
+      this.rejectList.push(fcb);
+    }
+    return this;
+  }
+  catch(fcb) {
+    if (fcb) {
+      this.rejectList.push(fcb);
+    }
+    return this;
+  }
+  resolve(data) {
+    if (this.status !== "pending") {
+      return;
+    }
+    this.status = "fulfilled";
+    setTimeout(() => {
+      this.resolveList.forEach((s) => {
+        s(data);
+      });
+    });
+  }
+  reject(err) {
+    if (this.status !== "pending") {
+      return;
+    }
+    this.status = "rejected";
+    setTimeout(() => {
+      this.rejectList.forEach((f) => {
+        f(err);
+      });
+    });
+  }
+  static resolve(data) {
+    if (data instanceof Promise) {
+      return data;
+    } else {
+      resolve(data);
+    }
+  }
+  static reject(err) {
+    if (err instanceof Promise) {
+      return err;
+    } else {
+      reject(err);
+    }
+  }
+  static all(promises) {
+    return new Promise((resolve, reject) => {
+      promises = Array.from(promises);
+      let count = 0;
+      let length = promises.length;
+      let result = [];
+      for (let i = 0; i < length; i++) {
+        Promise.resolve(promises[i]).then(
+          (data) => {
+            result[i] = data;
+            ++count;
+            if (count === length) {
+              resolve(result);
+            }
+          },
+          (err) => {
+            reject(err);
+            return;
+          }
+        );
+      }
+    });
+  }
+
+  static race(promises) {
+    return new Promise((resolve, reject) => {
+      promises = Array.from(promises);
+      let length = promises.length;
+      for (let i = 0; i < length; i++) {
+        Promise.resolve(promises[i]).then(
+          (data) => {
+            resolve(data);
+            return;
+          },
+          (err) => {
+            reject(err);
+            return;
+          }
+        );
+      }
+    });
+  }
 }
